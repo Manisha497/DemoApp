71bc6c9c1cd6523d2d87dfbf16f19e5621c10bbe
Manisha <manishawaje64@gmail.com>
2022-11-10 14:06:58 +0530

    Second commit

--- DemoApp/src/io/ineuron/HelloWorld.java
+++ DemoApp/src/io/ineuron/HelloWorld.java
@@ -4,6 +4,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		int[] arr= {1,4,5,2,3};
+		System.out.println("array length: "+arr.length);
+		for(int ele:arr) {
+			System.out.print(ele+" ");
+		}
 
 	}
 
