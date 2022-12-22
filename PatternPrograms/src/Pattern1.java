8f1fb3c10be371889710938ef1bb575e294b95f0
Manisha <manishawaje64@gmail.com>
2022-12-22 12:21:39 +0530

    Pattern programming

---
+++ PatternPrograms/src/Pattern1.java
@@ -0,0 +1,20 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		int n=12;
+		
+		//X with border
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(i==j || i+j==n-1 || i==0 ||i==n-1 || j==0 || j==n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
